a946628e364833edb83f5639e675fea70f45ec3d
Michael Chang <dks50217@gmail.com>
2020-09-27 21:49:58 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Google_Speech_to_Text
+
+> Google STT 與 FFmpeg使用範例
+
+
+
+
+
+
+
+
---
+++ model/param_gcp.py
@@ -0,0 +1,11 @@
+class Param_GCP():
+
+    def __init__(self, listFiles, outputFolder, langCode,
+                boolOpenOutputFilesAuto,langCodeList):
+        self.listFiles = listFiles
+        self.outputFolder = outputFolder
+        self.langCode = langCode
+        
+
+
+
